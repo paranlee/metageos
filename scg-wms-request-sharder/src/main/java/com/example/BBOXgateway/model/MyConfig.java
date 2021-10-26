6a4dfa6c6a791f4859962efd93a8dd400fc9d36d
paranlee <p4ranlee@gmail.com>
2021-10-26 17:02:10 +0900

    Fix routing bugs and dns routed uri

--- scg-wms-request-sharder/src/main/java/com/example/BBOXgateway/model/MyConfig.java
+++ scg-wms-request-sharder/src/main/java/com/example/BBOXgateway/model/MyConfig.java
@@ -1,14 +1,10 @@
 package com.example.BBOXgateway.model;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "route")
